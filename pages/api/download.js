352a104e268118f4e88d87257d660f70aa163ba3
David <davidmcnamee314@gmail.com>
2020-08-19 20:21:56 -0400

    with ffmpeg installed

--- pages/api/download.js
+++ pages/api/download.js
@@ -1,9 +1,12 @@
 const youtubedl = require("youtube-dl");
+const ffmpeg = require("@ffmpeg-installer/ffmpeg");
+
+console.log("ffmpeg path:", ffmpeg.path);
 
 export default (req, res) => {
   const video = youtubedl(
     "http://www.youtube.com/watch?v=90AiXO1pAiA",
-    ["-x", "--audio-format", "mp3"],
+    ["-x", "--audio-format", "mp3", "--ffmpeg-location", ffmpeg.path],
     {}
   );
 
